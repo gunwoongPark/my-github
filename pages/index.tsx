afcba828ba6b607e2a10f721787c44febdf89f8d
gunwoongPark <bung1438@gmail.com>
2022-12-22 05:21:35 +0900

    feat: getStaticProps 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { useEffect } from "react";
 import styled from "styled-components";
+import userApi from "../lib/api/user";
+import { UserRes } from "../lib/api/user/schema";
 
-export default function HomePage() {
+export default function HomePage(
+  props: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  useEffect(() => {
+    console.log(props.userInformation);
+  }, [props.userInformation]);
+
   return <H1>HELLO NEXT</H1>;
 }
 
@@ -8,8 +18,20 @@ const H1 = styled.h1`
   color: blue;
 `;
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps<{
+  userInformation: UserRes;
+}> = async () => {
+  const userInformation = await userApi.fetchUser();
+
+  if (!userInformation) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
-    props: {},
+    props: {
+      userInformation,
+    },
   };
-}
+};
